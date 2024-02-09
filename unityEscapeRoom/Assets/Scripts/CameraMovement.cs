1138b52955ee04f41e5a3636f6ee74375c5e132d
Moojin <mahn0318@gmail.com>
2024-02-08 21:22:13 -0500

    Added initial camera position/rotation and camera lock

--- unityEscapeRoom/Assets/Scripts/CameraMovement.cs
+++ unityEscapeRoom/Assets/Scripts/CameraMovement.cs
@@ -8,22 +8,37 @@ public class MouseLookAround : MonoBehaviour
     public float sensitivity = 10.0f;
     public Vector3 initialRotation = new Vector3(0f, 180f, 0f); // Initial starting rotation
 
+    // Reference to the clipboard script
+    public ClipBoard_Script clipboardScript;
+
     void Start()
     {
         // Set the initial rotation
         rotationX = initialRotation.x;
         rotationY = initialRotation.y;
         transform.localEulerAngles = initialRotation;
+
+        clipboardScript = FindObjectOfType<ClipBoard_Script>();
     }
 
     void Update()
     {
-        rotationY += Input.GetAxis("Mouse X") * sensitivity;
-        rotationX += Input.GetAxis("Mouse Y") * -1 * sensitivity;
+        // Check if the clipboard is not in front of the camera
+        if (clipboardScript != null && !clipboardScript.inFront)
+        {
+            rotationY += Input.GetAxis("Mouse X") * sensitivity;
+            rotationX += Input.GetAxis("Mouse Y") * -1 * sensitivity;
 
-        // Clamp vertical rotation to prevent flipping
-        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+            // Clamp vertical rotation to prevent flipping
+            rotationX = Mathf.Clamp(rotationX, -90f, 90f);
 
-        transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+            transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+        }
+        else
+        {
+            rotationX = initialRotation.x;
+            rotationY = initialRotation.y;
+            transform.localEulerAngles = initialRotation;
+        }
     }
 }
